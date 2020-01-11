f98f686637b4d5cb470c516e886cbb51dc14d03f
itknowledge4 <44100881+itknowledge4@users.noreply.github.com>
2020-01-11 18:23:03 +0200

    Add files via upload

--- Install Hyper-V on a Windows Server 2012 VM/commands.ps1
+++ Install Hyper-V on a Windows Server 2012 VM/commands.ps1
@@ -1,4 +1,4 @@
-#Important: You can install the Hyper-V role and management tools on a 2012/2012R2 VM but cannot run virtual machines because it does not guest support nested virtualization (if the VM is on Hyper-V)
+#Important: You can install the Hyper-V role and management tools on a 2012/2012R2 VM but cannot run virtual machines because it does not have guest support for nested virtualization (if the VM is on Hyper-V)
 
 Enable-WindowsOptionalFeature –Online -FeatureName Microsoft-Hyper-V –All -NoRestart
 Install-WindowsFeature RSAT-Hyper-V-Tools -IncludeAllSubFeature
